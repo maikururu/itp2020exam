95df3682203f66cc5cc7f45c8e97d7ff9f44a875
anewardeberg <anelan@hotmail.no>
2020-03-31 17:16:44 +0200

    wrote flexibility class

--- src/Exercise/Flexibility.java
+++ src/Exercise/Flexibility.java
@@ -1,4 +1,13 @@
 package Exercise;
 
 public class Flexibility extends Exercise {
+    public String requirement;
+
+    public void setRequirement(String _requirement) {
+        this.requirement = _requirement;
+    }
+
+    public String getRequirement(){
+        return requirement;
+    }
 }
