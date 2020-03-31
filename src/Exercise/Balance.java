0aee82b9e527920a0106468f40157c03258c8e9d
thelifeisyours <tliyours@gmail.com>
2020-03-31 16:28:41 +0200

    added ground variables to Exercise.java and sub classes

--- src/Exercise/Balance.java
+++ src/Exercise/Balance.java
@@ -1,6 +1,19 @@
 package Exercise;
 
 public class Balance extends Exercise {
+    public String equipment;
+
+    public void balance() {
+
+    }
+
+    public void setEquipment(String _equipment){
+        this.equipment = _equipment;
+    }
+
+    public String getEquipment(){
+        return equipment;
+    }
 }
 
 /*
--- src/Exercise/Exercise.java
+++ src/Exercise/Exercise.java
@@ -1,4 +1,8 @@
 package Exercise;
 
 public class Exercise {
+    public int intensity;
+    public int duration;
+    public int repetitions;
+    public int sets;
 }
