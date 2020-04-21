db832e4bffb2cf802a4b5d4d8ecd33d185b1d692
anewardeberg <anelan@hotmail.no>
2020-04-21 11:27:04 +0200

    wrote basic unit tests. Coverage = 100% in all subclasses

--- Tests/Test1.java
+++ Tests/Test1.java
@@ -1,14 +1,21 @@
 import org.junit.Test;
 import Exercise.*;
+import org.junit.experimental.theories.suppliers.TestedOn;
 
 import static org.junit.Assert.assertTrue;
 
 public class Test1 {
+    // Unit test?
     @Test
     public void testGetEquipment() {
-        Balance b1 = new Balance("Balanse", 10, 9, 8, 7, "Legs");
+        Balance b1 = new Balance("Atomic Situps", 3, 20, 30, 5, "Floor");
 
-        assertTrue(b1.getEquipment() == "Legs");
+        assertTrue(b1.getEquipment() == "Floor");
+
+    }
+
+    @Test
+    public void testToString() {
 
     }
 }
