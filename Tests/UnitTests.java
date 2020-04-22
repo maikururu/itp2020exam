1f5dacf46798078f811f2314c759cdae85f573c9
anewardeberg <anelan@hotmail.no>
2020-04-22 10:27:21 +0200

    cleaned up Unit Tests

--- Tests/UnitTests.java
+++ Tests/UnitTests.java
@@ -5,6 +5,16 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class UnitTests {
+    Exercise e = new Exercise("Exercise", 5, 10, 30, 5);
+    Exercise e2 = new Exercise("Exercise 2", 1, 10, 30, 5);
+
+    Endurance endurance = new Endurance("Body weight squats", 3, 5, 30, 3, "None");
+
+    Balance b = new Balance("Atomic Situps", 3, 20, 30, 5, "None");
+
+    Flexibility f = new Flexibility("Standing hamstring stretch", 3, 4, 2, 1, "Warm muscles");
+
+    Strength s = new Strength("Bench press", 5, 3, 10, 3, 30, "Weight bench");
 
     // Exercice class
     @Test
@@ -24,16 +34,13 @@ public class UnitTests {
 
     @Test
     public void testExerciseToString() {
-        Exercise e = new Exercise("Exercise", 5, 10, 30, 5);
         String expected =
                 "\n=============================\nExercise: \nName = Exercise\nIntensity = '5'\nDuration = 10\nRepetitions = 30\nSets = 5";
+        assertEquals(expected, e.toString());
     }
 
     @Test
     public void testExerciseCompareTo() {
-        Exercise e = new Exercise("Exercise", 5, 10, 30, 5);
-        Exercise e2 = new Exercise("Exercise 2", 1, 10, 30, 5);
-
         assertEquals(e.compareTo(e2), 4);
 
     }
@@ -41,7 +48,6 @@ public class UnitTests {
     // Balance class
     @Test
     public void testBalanceClass() {
-        Balance b = new Balance("Atomic Situps", 3, 20, 30, 5, "None");
         // Testing getEquipment()
         assertEquals(b.getEquipment(),"None");
 
@@ -49,7 +55,6 @@ public class UnitTests {
 
     @Test
     public void testBalanceToString() {
-        Balance b = new Balance("Atomic Situps", 3, 20, 30, 5, "None");
         String expected =
                 "\n=============================\nExercise: \nName = Atomic Situps\nIntensity = '3'\nDuration = 20\nRepetitions = 30\nSets = 5\nEquipment = None";
 
@@ -59,25 +64,22 @@ public class UnitTests {
     // Endurance class
     @Test
     public void testEnduranceClass() {
-        Endurance e = new Endurance("Body weight squats", 3, 5, 30, 3, "None");
         // Testing getEquipment()
-        assertEquals(e.getEquipment(),"None");
+        assertEquals(endurance.getEquipment(),"None");
 
     }
 
     @Test
     public void testEnduranceToString() {
-        Endurance e = new Endurance("Body weight squats", 3, 5, 30, 3, "None");
         String expected =
                 "\n=============================\nExercise: \nName = Body weight squats\nIntensity = '3'\nDuration = 5\nRepetitions = 30\nSets = 3\nEquipment = None";
 
-        assertEquals(expected, e.toString());
+        assertEquals(expected, endurance.toString());
     }
 
     // Flexibility class
     @Test
     public void testFlexibilityClass() {
-        Flexibility f = new Flexibility("Standing hamstring stretch", 3, 4, 2, 1, "Warm muscles");
         // Testing getRequirement()
         assertEquals(f.getRequirement(),"Warm muscles");
 
@@ -85,7 +87,6 @@ public class UnitTests {
 
     @Test
     public void testFlexibilityToString() {
-        Flexibility f = new Flexibility("Standing hamstring stretch", 3, 4, 2, 1, "Warm muscles");
         String expected =
                 "\n=============================\nExercise: \nName = Standing hamstring stretch\nIntensity = '3'\nDuration = 4\nRepetitions = 2\nSets = 1\nRequirement = Warm muscles";
 
@@ -95,7 +96,6 @@ public class UnitTests {
     // Strength class
     @Test
     public void testStrengthClass() {
-        Strength s = new Strength("Bench press", 5, 3, 10, 3, 30, "Weight bench");
         // Testing getWeights()
         assertEquals(s.getWeights(),30);
 
@@ -106,7 +106,6 @@ public class UnitTests {
 
     @Test
     public void testStrengthToString() {
-        Strength s = new Strength("Bench press", 5, 3, 10, 3, 30, "Weight bench");
         String expected =
                 "\n=============================\nExercise: \nName = Bench press\nIntensity = '5'\nDuration = 3\nRepetitions = 10\nSets = 3\nWeights = 30\nEquipment = Weight bench";
 
